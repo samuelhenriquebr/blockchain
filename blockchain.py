5960c133e5b429190668c07dfd5ab09d4986b181
Samuel Gomes <samuelvicosa@gmail.com>
2022-06-08 09:09:08 -0300

    First commit

---
+++ blockchain.py
@@ -0,0 +1,22 @@
+# Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 - Building a Blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_has = '0')
+    
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
+# Part 2 - Mining our Blockchain
